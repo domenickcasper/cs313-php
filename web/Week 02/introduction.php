492389d6161868b299aad2dc58442d4438eacebd
Domenick Casper <domenicktcasper@gmail.com>
2019-04-29 20:24:42 -0600

    This looks great

--- web/Week 02/introduction.php
+++ web/Week 02/introduction.php
@@ -19,7 +19,7 @@
 
 	<h2>Hello Everyone, my name is Domenick Casper. I am
 		from a small town North of Sacramento, called Lincoln. 
-		I served my Mission in the New York, New York North Mission. I love New York so very much, and I can't wait to visit it again. Some of my hobbies include playing guitar, listening to music, watching movies, and my all time favorite, Playing Video Games. I play video games a lot more than I should but it all works out. I would love to play video games professionally, or I want to become a game developer. My favorite video game: Overwatch!
+		I served my Mission in the New York, New York North Mission. I love New York so very much, and I can't wait to visit it again. Some of my hobbies include playing guitar, listening to music, watching movies, and my all time favorite, Playing Video Games. I play video games a lot more than I should but it all works out. I would love to play video games professionally, or I want to become a game developer. I built my own gaming computer November of 2017, and I absolutely love it, I am definitly a PC gamer! My favorite video game: Overwatch! 
 	</h2>
 
 	<img src="overwatch.jpg" alt="One of my all time favorite video games">
