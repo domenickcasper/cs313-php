4153947c4357aeeb2a98914c6e4c600315521c2b
Domenick Casper <domenicktcasper@gmail.com>
2019-05-24 10:13:01 -0600

    Added a query

---
+++ web/Inventory/inventory.php
@@ -0,0 +1,23 @@
+<? php
+session_start();
+include 'database.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INVENTORY</title>
+</head>
+<body>
+
+<? php
+foreach ($db->query('SELECT email, password FROM users') as $row)
+{
+  echo 'email: ' . $row['email'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+?>
+
+</body>
+</html>
