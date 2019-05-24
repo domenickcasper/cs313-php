18c806937ba760233d51652d732780e7eb8b92b1
Domenick Casper <domenicktcasper@gmail.com>
2019-05-24 10:36:06 -0600

    hopefully added type

--- web/Inventory/inventory.php
+++ web/Inventory/inventory.php
@@ -10,9 +10,30 @@ session_start();
 </head>
 <body>
 
-<select name="type0">
+<p>Movies</p>
+<select name="movies">
 <?php
 foreach ($db->query('SELECT id, type FROM type WHERE media = 0') as $row)
+	{
+  	echo '<option value =' . $row['id'] . '>' . $row['type'] . '</option>';
+	}
+?>
+</select>
+
+<p>Video Games</p>
+<select name="console">
+<?php
+foreach ($db->query('SELECT id, console FROM console') as $row)
+	{
+  	echo '<option value =' . $row['id'] . '>' . $row['console'] . '</option>';
+	}
+?>
+</select>
+
+<p>Music</p>
+<select name="music">
+<?php
+foreach ($db->query('SELECT id, type FROM type WHERE media = 2') as $row)
 	{
   	echo '<option value =' . $row['id'] . '>' . $row['type'] . '</option>';
 	}
