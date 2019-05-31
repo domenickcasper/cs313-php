d7d80ad00b8d16daf7d6a1764fc953db4d346283
Domenick Casper <domenicktcasper@gmail.com>
2019-05-31 10:29:28 -0600

    Added a movie link to database

--- web/Inventory/inventory.php
+++ web/Inventory/inventory.php
@@ -48,7 +48,7 @@ foreach ($db->query('SELECT id, type FROM type WHERE media = 0') as $row)
 
 <input type="submit" value="Submit">
 </form>
-<a href = "./movie.php">View Movie Inventory>
+<a href = "./movies.php">Movie Inventory</a>
 
 <!--Video Game Form-->
 <form action="console.php" method="POST">
