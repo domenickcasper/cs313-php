4f1fea0f1a0dc06af1430507cdb5d4617fcb20f7
Domenick Casper <domenicktcasper@gmail.com>
2019-05-31 09:55:36 -0600

    added shtuff

--- web/Inventory/movies.php
+++ web/Inventory/movies.php
@@ -2,26 +2,30 @@
 session_start();
 include 'database.php';
 if (isset($_POST)) {
-	$movieT = $_POST['movieTitle'];
-	$movieS = $_POST['movieSubTitle'];
-	$movieR = $_POST['movieRating'];
-	$movieG = $_POST['movieGenre'];
-	$movieTy = $_POST['movieType'];
-	$user = 1;
+	if (isset($_POST['delete'])) {
+		$db->query('DELETE FROM movies WHERE id =' . $_POST['delete']);
+	}
+	else {
+		$movieT = $_POST['movieTitle'];
+		$movieS = $_POST['movieSubTitle'];
+		$movieR = $_POST['movieRating'];
+		$movieG = $_POST['movieGenre'];
+		$movieTy = $_POST['movieType'];
+		$user = 1;
 
-	$sql = 'INSERT INTO movies (title, subtitle, rating, genre, type, user_id)
-				VALUES (:title, :subtitle, :rating, :genre, :type, :user_id)';
+		$sql = 'INSERT INTO movies (title, subtitle, rating, genre, type, user_id)
+					VALUES (:title, :subtitle, :rating, :genre, :type, :user_id)';
 
-	$prep = $db->prepare($sql);
-	$prep->bindParam(':title', $movieT);
-	$prep->bindParam(':subtitle', $movieS);
-	$prep->bindParam(':rating', $movieR);
-	$prep->bindParam(':genre', $movieG);
-	$prep->bindParam(':type', $movieTy);
-	$prep->bindParam(':user_id', $user);
+		$prep = $db->prepare($sql);
+		$prep->bindParam(':title', $movieT);
+		$prep->bindParam(':subtitle', $movieS);
+		$prep->bindParam(':rating', $movieR);
+		$prep->bindParam(':genre', $movieG);
+		$prep->bindParam(':type', $movieTy);
+		$prep->bindParam(':user_id', $user);
 
-	$prep->execute();
-
+		$prep->execute();
+	}
 }
 ?>
 
@@ -32,6 +36,7 @@ if (isset($_POST)) {
 </head>
 <body>
 
+<form method="POST" action="movies.php">
 <table>
 	<tr>
 		<th>Title</th>
@@ -41,7 +46,7 @@ if (isset($_POST)) {
 		<th>Type</th>
 	</tr>
 <?php
-	foreach ($db->query('SELECT m.title, m.subtitle, g.genre, t.type, r.rating FROM movies m
+	foreach ($db->query('SELECT m.id, m.title, m.subtitle, g.genre, t.type, r.rating FROM movies m
 	INNER JOIN rating r ON m.rating = r.id INNER JOIN type t ON m.type = t.id 
 	INNER JOIN genre g ON m.genre = g.id WHERE m.user_id = 1') as $row) {
 		echo '<tr>';
@@ -50,10 +55,12 @@ if (isset($_POST)) {
 		echo '<td>' . $row['rating'] . '</td>';
 		echo '<td>' . $row['genre'] . '</td>';
 		echo '<td>' . $row['type'] . '</td>';
+		echo '<td><button type="submit" value="' . $row['id'] . '"name="delete">Delete Items</button>'; 
 		echo '</tr>';
 	}
 ?>
 </table>
+</form>
 
 </body>
 </html>
