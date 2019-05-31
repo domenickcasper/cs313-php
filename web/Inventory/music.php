14f5c444bb44776eb30bf0765599fa048a4c7fb0
Domenick Casper <domenicktcasper@gmail.com>
2019-05-31 10:16:10 -0600

    Added music table

--- web/Inventory/music.php
+++ web/Inventory/music.php
@@ -1,6 +1,32 @@
 <?php
 session_start();
+include 'database.php';
+if (isset($_POST)) {
+	if (isset($_POST['delete'])) {
+		$db->query('DELETE FROM music WHERE id =' . $_POST['delete']);
+	}
+	else {
+		$musicTitle = $_POST['musicTitle'];
+		$musicArtist = $_POST['artist'];
+		$musicAlbum = $_POST['album'];
+		$musicGenre = $_POST['musicGenre'];
+		$musicType = $_POST['music'];
+		$user = 1;
 
+		$sql = 'INSERT INTO music (title, artist, album, genre, type, user_id)
+					VALUES (:title, :artist, :album, :genre, :type, :user_id)';
+
+		$prep = $db->prepare($sql);
+		$prep->bindParam(':title', $musicTitle);
+		$prep->bindParam(':artist', $musicArtist);
+		$prep->bindParam(':album', $musicAlbum);
+		$prep->bindParam(':genre', $musicGenre);
+		$prep->bindParam(':type', $musicType);
+		$prep->bindParam(':user_id', $user);
+
+		$prep->execute();
+	}
+}
 ?>
 
 <!DOCTYPE html>
