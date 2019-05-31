61b54ed4668c872c9790bb4062db653849b4811c
Domenick Casper <domenicktcasper@gmail.com>
2019-05-31 10:20:18 -0600

    Added music table

--- web/Inventory/music.php
+++ web/Inventory/music.php
@@ -35,6 +35,30 @@ if (isset($_POST)) {
 	<title>Music</title>
 </head>
 <body>
-
+<form method="POST" action="music.php">
+<table>
+	<tr>
+		<th>Title</th>
+		<th>Artist</th>
+		<th>Album</th>
+		<th>Genre</th>
+		<th>Type</th>
+	</tr>
+<?php
+	foreach ($db->query('SELECT m.id, m.title, m.artist, m.album, g.genre, t.type FROM music m
+	INNER JOIN type t ON m.type = t.id 
+	INNER JOIN genre g ON m.genre = g.id WHERE m.user_id = 1') as $row) {
+		echo '<tr>';
+		echo '<td>' . $row['title'] . '</td>';
+		echo '<td>' . $row['artist'] . '</td>';
+		echo '<td>' . $row['album'] . '</td>';
+		echo '<td>' . $row['genre'] . '</td>';
+		echo '<td>' . $row['type'] . '</td>';
+		echo '<td><button type="submit" value="' . $row['id'] . '"name="delete">Delete Items</button>'; 
+		echo '</tr>';
+	}
+?>
+</table>
+</form>
 </body>
 </html>
