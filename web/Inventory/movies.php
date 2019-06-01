ba0f385e899b120faeffa61441138192b28d862b
Domenick Casper <domenicktcasper@gmail.com>
2019-06-01 17:53:27 -0600

    Hopeful Get an update Button

--- web/Inventory/movies.php
+++ web/Inventory/movies.php
@@ -55,7 +55,7 @@ if (isset($_POST)) {
 		echo '<td>' . $row['rating'] . '</td>';
 		echo '<td>' . $row['genre'] . '</td>';
 		echo '<td>' . $row['type'] . '</td>';
-		echo '<td><button type="submit" value="' . $row['id'] . '"name="update">Update</button>'; 
+		echo '<td><button value="' . $row['id'] . '"name="update">Update</button>'; 
 		echo '<td><button type="submit" value="' . $row['id'] . '"name="delete">Delete</button>';
 		echo '</tr>';
 	}
