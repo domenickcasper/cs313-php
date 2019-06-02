0e3fc9d4e4c968dcad7dd70a04cfd79578d20b61
Domenick Casper <domenicktcasper@gmail.com>
2019-06-01 18:06:17 -0600

    Sort

--- web/Inventory/movies.php
+++ web/Inventory/movies.php
@@ -51,7 +51,7 @@ if (isset($_POST)) {
 <?php
 	foreach ($db->query('SELECT m.id, m.title, m.subtitle, g.genre, t.type, r.rating FROM movies m 
 	INNER JOIN rating r ON m.rating = r.id INNER JOIN type t ON m.type = t.id 
-	INNER JOIN genre g ON m.genre = g.id WHERE m.user_id = 1 ORDER BY m.title') as $row) {
+	INNER JOIN genre g ON m.genre = g.id WHERE m.user_id = 1 ORDER BY m.title AND t.type') as $row) {
 		echo '<tr>';
 		echo '<td>' . $row['title'] . '</td>';
 		echo '<td>' . $row['subtitle'] . '</td>';
